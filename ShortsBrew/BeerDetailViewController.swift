c49fd29d167227ce616d58b78f687e8c0a4ac294
Brian Glowe <brianglowe@gmail.com>
2016-03-12 19:21:16 -0500

    Set up new VC for the beer details with segue, label, and BeerDetailViewcontroller

---
+++ ShortsBrew/BeerDetailViewController.swift
@@ -0,0 +1,39 @@
+//
+//  BeerDetailViewController.swift
+//  ShortsBrew
+//
+//  Created by Brian J Glowe on 3/12/16.
+//  Copyright © 2016 BG. All rights reserved.
+//
+
+import UIKit
+
+class BeerDetailViewController: UIViewController {
+    
+    @IBOutlet weak var beerLabel: UILabel!
+    
+    var beerFromSegue: String?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
